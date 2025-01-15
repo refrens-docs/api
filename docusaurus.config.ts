81eebcbd919fe7d956bfe334fa452a4918d447aa
Badal Saibo <badalsaibo@gmail.com>
2025-01-15 14:58:25 +0530

    refactor: cleanup

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,7 +53,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo.png',
     navbar: {
       title: 'Refrens Documentation',
       logo: {
