0f8c9eabb91d80ee64246e7a136939b674fa606d
Badal Saibo <badalsaibo@gmail.com>
2025-01-15 15:47:08 +0530

    refactor: rearrangement

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,8 +39,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/refrens-docs/api',
           routeBasePath: '/',
         },
         blog: false,
