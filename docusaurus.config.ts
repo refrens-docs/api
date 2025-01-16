394cc8b06c517bc9347896604dcc53592d7d96b5
Badal Saibo <badalsaibo@gmail.com>
2025-01-16 11:46:18 +0530

    chore: deployment branch change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   projectName: 'api', // Usually your repo name.
   trailingSlash: false,
   onBrokenLinks: 'throw',
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   onBrokenMarkdownLinks: 'warn',
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
